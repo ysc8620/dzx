4c029d75d5b00641310ab7ce076bdd3925c6d9b5
ysc8620 <ysc8620@163.com>
2016-06-23 14:39:25 +0800

    ds

--- Application/Common/Conf/db_config.php
+++ Application/Common/Conf/db_config.php
@@ -2,7 +2,7 @@
    // 'DB_DEPLOY_TYPE'=> true,
     'DB_TYPE' => 'mysql',
     'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'db_hd_v4',
+    'DB_NAME' => 'dzx',
     'DB_USER' => 'root',
     'DB_PWD' => '123456',
   //  'DB_RW_SEPARATE'=>true,
