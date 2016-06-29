bf5dd309682b01ed6282840f1912cd3716e5ebd4
ShengYue <ysc8620@163.com>
2016-06-29 18:31:18 +0800

    ds

--- Public/admin/js/base.js
+++ Public/admin/js/base.js
@@ -7,7 +7,31 @@ function credit(user_id,session_id){
         dataType:"json",
         async:false,
         success: function(msg){
-            alert("成功~");
+            if(msg.msg_code == 1000){
+
+                alert(msg.msg_content);
+            }else{
+                alert(msg.msg_content);
+            }
+        }
+    });
+}
+
+// 兑换记录
+function exchange(goods_id,session_id){
+    $.ajax({
+        type: "POST",
+        url: "/index.php?s=/user/exchange.html",
+        data: "goods_id="+goods_id+"&sess_id="+session_id,
+        dataType:"json",
+        async:false,
+        success: function(msg){
+            if(msg.msg_code == 1000){
+
+                alert(msg.msg_content);
+            }else{
+                alert(msg.msg_content);
+            }
         }
     });
 }
